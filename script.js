d4ab62e7f7f797890a833c063b10075cd5b81882
Vinicius Lopes <viniciuslopesdasilva01@gmail.com>
2023-11-27 22:21:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const navbar = document.querySelector('.navbar')
+const closeNav = document.querySelector('.bg-x')
+
+
+function vem(){
+    navbar.style.top = '0'
+    closeNav.style.top = '0'
+}
+
+
+function vai(){
+    navbar.style.top = '-150px'
+    closeNav.style.top = '-50px'
+}
